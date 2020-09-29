a5ae10cf5e27b0f390827c845ade46c0f38d1dea
MissaelAnda <hector.handa@alumnos.udg.mx>
2020-09-29 12:26:38 -0500

    prueba04

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var presicion int
+	var e float32 = 0.0
+
+	fmt.Scan(&presicion)
+
+	for i := 0; i <= presicion; i++ {
+		e += 1.0 / float32(factorial(uint(i)))
+	}
+
+	fmt.Println(e)
+}
+
+func factorial(n uint) uint {
+	if n == 0 {
+		return 1
+	}
+	var f uint = 1
+	var i uint
+	for i = 1; i <= n; i++ {
+		f *= i
+	}
+	return f
 }
